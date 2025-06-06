7ea1e08d279e1fef80de7f2d0fc99109ab23118a
aaronwai <aaron.lung.wai@gmail.com>
2025-06-06 12:23:37 +0800

    step 14 : dashboard

--- bcre/settings.py
+++ bcre/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,10 @@ MESSAGE_TAGS = {
     messages.ERROR : 'danger',
     messages.SUCCESS : 'success'
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True    
